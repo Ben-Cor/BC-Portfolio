b5fdd9cd3ab7baa6a06e8e6b15fd3cfafd419ecd
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-01-27 10:55:03 +0000

    Header html, tailwind  and JS added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const drop = document.querySelector('#dropdown');
+
+drop.addEventListener('mouseenter', show);
+function show() {
+    const menu = document.querySelector('#dropdown-content');
+    menu.classList.remove('hidden')
+}
+
+drop.addEventListener('mouseleave', hide);
+function hide() {
+    const menu = document.querySelector('#dropdown-content');
+    const drop = document.querySelector('.dropdown');
+    menu.classList.add('hidden')
+}
