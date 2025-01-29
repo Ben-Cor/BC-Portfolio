6b6132b28a28a23466b781303a37df10314a2e85
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-01-29 15:57:41 +0000

    Layout amendments and scroll jS attempts(commented out)

--- script.js
+++ script.js
@@ -69,6 +69,29 @@ function typeWriter2() {
 
 typeWriter();
 
+// typewriter about
+
+// var characterAbout = 0;
+// const txtAbout = 'About';
+// const aboutText = document.getElementById("introHeading")
+
+// function typeWriterAbout() {
+//     if (characterAbout < txtAbout.length){
+//     aboutText.textContent += txtAbout.charAt(characterAbout);
+//     characterAbout++;
+//     setTimeout(typeWriterAbout, 100);}
+//   }
+
+//   function clearAbout (){
+//     txtAbout.textContent = "";
+//   }
+
+// aboutText.addEventListener("focusIn", typeWriterAbout);
+// aboutText.addEventListener('blur', clearAbout)
+
+// typeWriterAbout();
+
+
 
 // json import and grid items
 
@@ -85,7 +108,7 @@ fetch('./Projects/projects.json')
         mainDiv.classList.add("inline-block", "relative")
 
         const hoverDiv = document.createElement("div");
-        hoverDiv.classList.add("md:absolute", "w-[100%]", "inset-0", "flex", "flex-col", "items-center", "justify-center", "md:opacity-0", "md:hover:opacity-100", "md:bg-amber-50", "pt-8", "md:bg-opacity-90", "transition-opacity", "duration-550", "rounded-xl")
+        hoverDiv.classList.add("md:absolute", "w-[100%]", "inset-0", "flex", "flex-col", "items-center", "justify-center", "text-center", "md:opacity-0", "md:hover:opacity-100", "md:bg-amber-50", "pt-8", "md:bg-opacity-90", "transition-opacity", "duration-550", "rounded-xl")
         
 
         const projectPhoto = document.createElement("img");
@@ -114,3 +137,38 @@ fetch('./Projects/projects.json')
         });
     })
   
+
+
+    // scroll background
+
+
+
+// var bg1 = "linear-gradient(90deg, rgba(255,251,235,0.99) 44%, rgba(254,243,199,0.99) 100%)";
+// var bg1_5 = "rgb(255,251,235)"
+// var bg2 = "linear-gradient(90deg, rgba(30,41,59,1) 20%, rgba(15,23,42,1) 95%)";
+// var bg3 = "linear-gradient(90deg, rgba(147,197,253,1) 20%, rgba(96,165,250,1) 95%)";
+// const body = document.querySelector("body")
+
+// document.addEventListener("scroll touchmove", () => {
+//   if ((document).scrollTop() >= document.getElementById("intro").position().top) {
+//     body.style.background = bg2;
+
+// }
+// else if ((document).scrollTop() >= document.getElementById("career").position().top) {
+//     body.style.background = bg3;
+
+// }
+// else if ((document).scrollTop() >= document.getElementById("about").position().top) {
+//   body.style.background = bg2;
+
+// }
+// else if ((document).scrollTop() >= document.getElementById("contact").position().top) {
+//   body.style.background = bg3;
+
+// }
+// else {
+//     body.style.background = bg1
+// }
+// });
+
+
